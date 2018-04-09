f15d6e8a19b8ab9ed1130d9f98425a00cab5e686
cornflower <861281331@qq.com>
2018-04-09 22:16:21 +0800

    增加图片

--- app/src/main/java/com/qingmang/App.java
+++ app/src/main/java/com/qingmang/App.java
@@ -1,10 +1,19 @@
 package com.qingmang;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.qingmang.baselibrary.utils.AppUtils;
 import com.qingmang.baselibrary.utils.ForegroundCallbacks;
 import com.qlzgzg.network.RetrofitServiceManager;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreater;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreater;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 
 /**
  * Created by xiejingbao on 2017/9/14.
@@ -26,6 +35,24 @@ public class App extends Application {
         return foregroundCallbacks;
     }
 
+    static {
+        //设置全局的Header构建器
+        SmartRefreshLayout.setDefaultRefreshHeaderCreater(new DefaultRefreshHeaderCreater() {
+            @Override
+            public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
+//                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
+                return new ClassicsHeader(context);
+            }
+        });
+        //设置全局的Footer构建器
+        SmartRefreshLayout.setDefaultRefreshFooterCreater(new DefaultRefreshFooterCreater() {
+            @Override
+            public RefreshFooter createRefreshFooter(Context context, RefreshLayout layout) {
+                //指定为经典Footer，默认是 BallPulseFooter
+                return new ClassicsFooter(context);
+            }
+        });
+    }
     @Override
     public void onCreate() {
         super.onCreate();
@@ -41,5 +68,7 @@ public class App extends Application {
 
     }
 
-
+    public RetrofitServiceManager getRetrofitServiceManager() {
+        return retrofitServiceManager;
+    }
 }
