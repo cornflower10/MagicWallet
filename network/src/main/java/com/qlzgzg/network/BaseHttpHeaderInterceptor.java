847f704000f21b654fff501f3ef9ef756881b729
caomingyu <361786231@qq.com>
2018-04-10 13:41:19 +0800

    请求头

--- network/src/main/java/com/qlzgzg/network/BaseHttpHeaderInterceptor.java
+++ network/src/main/java/com/qlzgzg/network/BaseHttpHeaderInterceptor.java
@@ -6,19 +6,13 @@ import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
-/**
- * Created by qlzg-cmy on 2017/5/5.
- */
 
 public class BaseHttpHeaderInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request original = chain.request();
         Request request = original.newBuilder()
-                .addHeader("X-App-Type", "android")
-                .addHeader("X-App-Version", String.valueOf(BuildConfig.VERSION_NAME))
-                .addHeader("X-App-Env", "dev")
-                .addHeader("X-App-Key", "android")
+                .addHeader("token", "f98aef708af140948e6962ab0a215333")
                 .method(original.method(), original.body())
                 .build();
         return chain.proceed(request);
