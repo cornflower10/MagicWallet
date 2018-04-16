071b19453f7b6a1383cbbd47cbf6d6782e3f66eb
cornflower <861281331@qq.com>
2018-04-16 20:48:34 +0800

    热门银行卡详情

---
+++ app/src/main/java/com/qingmang/bank/CreditCardInfoActivity.java
@@ -0,0 +1,15 @@
+package com.qingmang.bank;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qingmang.R;
+
+public class CreditCardInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card_info);
+    }
+}
